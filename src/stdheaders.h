1e5cd779afd91659d9be3d16bdb8a0c507e1e9e9
Selvin Dhavidhu <selvin.dhavidhu@gmail.com>
2025-01-20 23:05:06 +0530

    Added few more standard headers such as stdexcept and numeric.

--- src/stdheaders.h
+++ src/stdheaders.h
@@ -4,10 +4,13 @@
 #include <string>
 #include <algorithm>
 #include <fstream>
+#include <sstream>
 #include <unordered_map>
 #include <vector>
 #include <iostream>
 #include <iomanip>
+#include <stdexcept>
+#include <numeric>
 
 #endif // !SELVIN_AOC2024_STDHEADERS_H_INCLUDED
 
