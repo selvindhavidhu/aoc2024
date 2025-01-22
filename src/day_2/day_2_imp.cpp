a636e5d18d000938703f767e026e403380c7ae15
Selvin Dhavidhu <selvin.dhavidhu@gmail.com>
2025-01-23 01:16:30 +0530

    Solved Part 2 of Day 2.

--- src/day_2/day_2_imp.cpp
+++ src/day_2/day_2_imp.cpp
@@ -10,12 +10,47 @@ static bool is_decreasing(const std::vector<int>& vec) {
 }
 
 static bool is_in_range(int value) {
-	return (value >= 1 && value <= 3) || (value <= -1 && value >= -3);
+	value = std::abs(value);
+	return (value >= 1 && value <= 3);
+}
+
+static bool is_safe(const std::vector<int>& levels) {
+	// Edge case: list has only 1 element
+	auto size = levels.size();
+	if (size < 2) return true;
+
+	// Edge case: list has only 2 elements
+	if (size == 2) return is_in_range(levels[0] - levels[1]);
+
+	// Assert that the list is in either increasing or decreasing order
+	std::vector<int> differences(size);
+	if (is_increasing(levels) || is_decreasing(levels)) {
+		// Assert that the difference between adjacent levels is in the range [1, 3].
+		std::adjacent_difference(std::begin(levels), std::end(levels), std::begin(differences));
+		if (std::all_of(std::begin(differences) + 1, std::end(differences), is_in_range)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+static bool is_safe_after_removing_single_level(const std::vector<int>& levels) {
+	if (is_safe(levels)) return true;
+
+	// remove one element at a time and check if the list is safe.
+	size_t size = levels.size();
+	for (size_t i = 0; i < size; ++i) {
+		std::vector<int> temp(std::begin(levels), std::end(levels));
+		temp.erase(temp.begin() + i);
+		if (is_safe(temp)) return true;
+	}
+
+	return false;
 }
 
 int solve_day_2_part_1(const char* input_file_path) {
-	int unsafe_count = 0;
-	int total = 0;
+	int nsafe = 0;
 
 	std::ifstream input_file(input_file_path);
 	if (!input_file.is_open()) {
@@ -26,27 +61,46 @@ int solve_day_2_part_1(const char* input_file_path) {
 
 	std::string line;
 	while (std::getline(input_file, line)) {
-		++total;
 		std::istringstream iss(line);
-		std::vector<int> numbers;
+		std::vector<int> levels;
 		
 		int number;
 		while (iss >> number) {
-			numbers.push_back(number);
+			levels.push_back(number);
 		}
 
-		std::vector<int> differences(numbers.size());
-		if (is_increasing(numbers) || is_decreasing(numbers)) {
-			std::adjacent_difference(numbers.begin(), numbers.end(), differences.begin());
-			if (!std::all_of(differences.begin() + 1, differences.end(), is_in_range)) {
-				++unsafe_count;
-			}
-		} else {
-			++unsafe_count;
-		}
+		if (is_safe(levels)) ++nsafe;
 
 	}
 	input_file.close();
 
-	return (total - unsafe_count);
+	return nsafe;
+}
+
+int solve_day_2_part_2(const char* input_file_path) {
+	int nsafe = 0;
+
+	std::ifstream input_file(input_file_path);
+	if (!input_file.is_open()) {
+		std::stringstream error_message;
+		error_message << "Unable to open file: " << input_file_path << '\n';
+		throw std::runtime_error(error_message.str());
+	}
+
+	std::string line;
+	while (std::getline(input_file, line)) {
+		std::istringstream iss(line);
+		std::vector<int> levels;
+
+		int number;
+		while (iss >> number) {
+			levels.push_back(number);
+		}
+
+		if (is_safe_after_removing_single_level(levels)) ++nsafe;
+
+	}
+	input_file.close();
+
+	return nsafe;
 }
